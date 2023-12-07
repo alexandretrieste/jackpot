558f023f5041b4d82b7b48dec3b9b5e431cad221
Alexandre <alexandre.trieste@gmail.com>
2023-12-07 00:06:47 -0300

    feat: created some previous animation

--- project.js
+++ project.js
@@ -1,4 +1,5 @@
-const prompt = require("prompt-sync")();
+const canvas = document.getElementById("slotCanvas");
+const ctx = canvas.getContext("2d");
 
 const ROWS = 3;
 const COLS = 3;
@@ -17,43 +18,65 @@ const SYMBOL_VALUES = {
   D: 2,
 };
 
-const deposit = () => {
-  while (true) {
-    const depositAmount = prompt("Enter a deposit amount: ");
-    const numberDepositAmount = parseFloat(depositAmount);
+const symbolSize = 50;
 
-    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
-      console.log("Invalid deposit amount, try again.");
-    } else {
-      return numberDepositAmount;
+const symbolImages = {
+  A: new Image(),
+  B: new Image(),
+  C: new Image(),
+  D: new Image(),
+};
+
+symbolImages.A.src = 'images/symbolA.jpg';
+symbolImages.B.src = 'images/symbolB.jpg';
+symbolImages.C.src = 'images/symbolC.jpg';
+symbolImages.D.src = 'images/symbolD.jpg';
+
+const depositAmountInput = document.getElementById("depositAmount");
+const numberOfLinesInput = document.getElementById("numberOfLines");
+const betPerLineInput = document.getElementById("betPerLine");
+
+const drawSymbol = (symbol, x, y) => {
+  ctx.drawImage(symbolImages[symbol], x, y, symbolSize, symbolSize);
+};
+
+const drawReels = (reels) => {
+  for (let i = 0; i < COLS; i++) {
+    for (let j = 0; j < ROWS; j++) {
+      const x = i * symbolSize;
+      const y = j * symbolSize;
+      drawSymbol(reels[i][j], x, y);
     }
   }
 };
 
-const getNumberOfLines = () => {
-  while (true) {
-    const lines = prompt("Enter the number of lines to bet on (1-3): ");
-    const numberOfLines = parseFloat(lines);
+const animateSpin = () => {
+  return new Promise((resolve) => {
+    const spinDuration = 2000;
+    const startTime = performance.now();
 
-    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
-      console.log("Invalid number of lines, try again.");
-    } else {
-      return numberOfLines;
-    }
-  }
-};
+    const spinFrame = (timestamp) => {
+      const elapsed = timestamp - startTime;
+      const progress = Math.min(elapsed / spinDuration, 1);
 
-const getBet = (balance, lines) => {
-  while (true) {
-    const bet = prompt("Enter the bet per line: ");
-    const numberBet = parseFloat(bet);
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
-      console.log("Invalid bet, try again.");
-    } else {
-      return numberBet;
-    }
-  }
+      for (let i = 0; i < COLS; i++) {
+        const x = i * symbolSize;
+        const y = canvas.height / 2 - symbolSize / 2;
+        const yOffset = Math.sin(progress * Math.PI) * 50;
+        drawSymbol("A", x, y + yOffset);
+      }
+
+      if (progress < 1) {
+        requestAnimationFrame(spinFrame);
+      } else {
+        resolve();
+      }
+    };
+
+    requestAnimationFrame(spinFrame);
+  });
 };
 
 const spin = () => {
@@ -92,19 +115,6 @@ const transpose = (reels) => {
   return rows;
 };
 
-const printRows = (rows) => {
-  for (const row of rows) {
-    let rowString = "";
-    for (const [i, symbol] of row.entries()) {
-      rowString += symbol;
-      if (i != row.length - 1) {
-        rowString += " | ";
-      }
-    }
-    console.log(rowString);
-  }
-};
-
 const getWinnings = (rows, bet, lines) => {
   let winnings = 0;
 
@@ -113,7 +123,7 @@ const getWinnings = (rows, bet, lines) => {
     let allSame = true;
 
     for (const symbol of symbols) {
-      if (symbol != symbols[0]) {
+      if (symbol !== symbols[0]) {
         allSame = false;
         break;
       }
@@ -127,30 +137,43 @@ const getWinnings = (rows, bet, lines) => {
   return winnings;
 };
 
-const game = () => {
-  let balance = deposit();
+const startGame = async () => {
+  const depositAmount = parseFloat(depositAmountInput.value);
+  const numberOfLines = parseInt(numberOfLinesInput.value);
+  const betPerLine = parseFloat(betPerLineInput.value);
 
-  while (true) {
-    console.log("You have a balance of $" + balance);
-    const numberOfLines = getNumberOfLines();
-    const bet = getBet(balance, numberOfLines);
-    balance -= bet * numberOfLines;
-    const reels = spin();
-    const rows = transpose(reels);
-    printRows(rows);
-    const winnings = getWinnings(rows, bet, numberOfLines);
-    balance += winnings;
-    console.log("You won, $" + winnings.toString());
+  if (isNaN(depositAmount) || isNaN(numberOfLines) || isNaN(betPerLine)) {
+    alert("Please enter valid numeric values.");
+    return;
+  }
 
-    if (balance <= 0) {
-      console.log("You ran out of money!");
-      break;
+  let balance = depositAmount;
+
+  console.log("You have a balance of $" + balance);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  await animateSpin();
+
+  const finalReels = spin();
+  drawReels(transpose(finalReels));
+
+  const winnings = getWinnings(transpose(finalReels), betPerLine, numberOfLines);
+  alert("You won, $" + winnings.toString());
+
+  balance += winnings;
+  console.log("Your updated balance is $" + balance);
+
+  if (balance <= 0) {
+    console.log("You ran out of money!");
+  } else {
+    const playAgain = confirm("Do you want to play again?");
+    if (playAgain) {
+      startGame();
+    } else {
+      console.log("Thanks for playing!");
     }
-
-    const playAgain = prompt("Do you want to play again (y/n)? ");
-
-    if (playAgain != "y") break;
   }
 };
 
-game();
+startGame();
